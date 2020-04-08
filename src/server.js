baa31802757c5f215b1c430ac6d83fbd52bfa7fb
rebeccaleighfoster <rebecca.leigh.foster@gmail.com>
2020-04-08 15:42:24 -0500

    deploying

--- src/server.js
+++ src/server.js
@@ -1,22 +1,13 @@
 const knex = require('knex')
 const app = require('./app')
-const { PORT, DATABASE_URL } = require('./config')
-
-console.log(process.env)
-/*
-const db = knex({
-  client: 'pg',
-   connection: DB_URL,
-   connection: DATABASE_URL,
-})
-*/
+const { PORT, DB_HOST, DB_PORT, DB_USER } = require('./config')
 
 const db = knex({
   client: 'pg',
   connection: {
-    host     : 'localhost',
-    port     : '5432',
-    user     : 'postgres', 
+    host     : DB_HOST || 'localhost',
+    port     : DB_PORT || '5432',
+    user     : DB_USER || 'postgres', 
     database : 'noteful',
     charset  : 'utf8',
     timezone : 'utc'
